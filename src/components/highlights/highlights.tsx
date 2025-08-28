015418155bc51b75aa374bed1393ab41968fcf60
nathsuki73 <nathsegovia73@gmail.com>
2025-08-28 22:55:02 +0800

    created bento initial structure

--- src/components/highlights/highlights.tsx
+++ src/components/highlights/highlights.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 const Highlights = () => {
   return (
-    <section className=' bg-amber-950'>
+    <section className=' h-screen'>
         <h1 className='font-bold text-3xl '>Featured Projects</h1>
-        <div>
-            
+        <div className='grid grid-cols-4 grid-rows-12 gap-4 bg-amber-800 w-1/2 h-1/2 '>
+            <div className='col-span-1 row-span-6 bg-amber-300'></div>
+            <div className='col-span-1 row-span-3 bg-amber-300'></div>
+            <div className='col-span-1 row-span-3 bg-amber-300'></div>
+            <div className='col-span-1 row-span-5 bg-amber-300'></div>
+            <div className='col-span-2 row-span-6 bg-amber-300'></div>
+            <div className='col-span-1 row-span-4 bg-amber-300'></div>
+            <div className='col-span-1 row-span-6 bg-amber-300'></div>
+            <div className='col-span-1 row-span-3 bg-amber-300'></div>
+            <div className='col-span-2 row-span-3 bg-amber-300'></div>
         </div>
 
     </section>
