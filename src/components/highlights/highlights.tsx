db6148e57f51d01e91305e540293adcf27c2ffaf
nathsuki73 <nathsegovia73@gmail.com>
2025-08-29 18:49:55 +0800

    updated the visuals of bento for minimalist style

--- src/components/highlights/highlights.tsx
+++ src/components/highlights/highlights.tsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 const Highlights = () => {
   return (
-    <section className=' h-screen'>
-        <h1 className='font-bold text-3xl '>Featured Projects</h1>
-        <div className='grid grid-cols-4 grid-rows-12 gap-4 bg-amber-800 w-1/2 h-1/2 '>
-            <div className='col-span-1 row-span-6 bg-amber-300'></div>
-            <div className='col-span-1 row-span-3 bg-amber-300'></div>
-            <div className='col-span-1 row-span-3 bg-amber-300'></div>
-            <div className='col-span-1 row-span-5 bg-amber-300'></div>
-            <div className='col-span-2 row-span-6 bg-amber-300'></div>
-            <div className='col-span-1 row-span-4 bg-amber-300'></div>
-            <div className='col-span-1 row-span-6 bg-amber-300'></div>
-            <div className='col-span-1 row-span-3 bg-amber-300'></div>
-            <div className='col-span-2 row-span-3 bg-amber-300'></div>
+    <section className='w-full h-screen my-24'>
+        <div className='flex p-5 w-full'>
+            <h1 className='font-bold text-3xl '>Highlights</h1>
+        </div>
+        <div className='grid grid-cols-4 grid-rows-12 gap-4 bg-background w-full h-full'>
+            <div className='col-span-1 row-span-6 border-2 border-foreground'></div>
+            <div className='col-span-1 row-span-3 border-2 border-foreground'></div>
+            <div className='col-span-1 row-span-3 border-2 border-foreground'></div>
+            <div className='col-span-1 row-span-5 border-2 border-foreground'></div>
+            <div className='col-span-2 row-span-6 border-2 border-foreground'></div>
+            <div className='col-span-1 row-span-4 border-2 border-foreground'></div>
+            <div className='col-span-1 row-span-6 border-2 border-foreground'></div>
+            <div className='col-span-1 row-span-3 border-2 border-foreground'></div>
+            <div className='col-span-2 row-span-3 border-2 border-foreground'></div>
         </div>
 
     </section>
