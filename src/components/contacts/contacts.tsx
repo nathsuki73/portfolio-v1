498f16a9a2dbf5d8c2efa4bb59d8a8a57aafe46c
nathsuki73 <nathsegovia73@gmail.com>
2025-08-31 22:00:14 +0800

    added container for contacts section

--- src/components/contacts/contacts.tsx
+++ src/components/contacts/contacts.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <div className='w-full my-24'>
+        <h1 className='text-9xl font-bold'>Contact Me.</h1>
+        <div className='grid grid-cols-2 grid-rows-4 gap-4'>
+            <div className='col-span-1 row-span-6 border-2 border-foreground'>
+            
+            </div>
+        </div>
+    </div>
   )
 }
 
