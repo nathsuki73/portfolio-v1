ffd578f38c33d421c305bbcdbb1e4579dcbf2e02
nathsuki73 <nathsegovia73@gmail.com>
2025-09-03 18:14:06 +0800

    created the mobile view of highlights

--- src/components/highlights/highlights.tsx
+++ src/components/highlights/highlights.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Highlights = () => {
   return (
-    <section className='w-full h-screen my-24'>
+    <section className='w-full h-auto my-24'>
         <div className='flex p-5 w-full'>
             <h1 className='font-bold text-3xl '>Highlights</h1>
         </div>
-        <div className='grid grid-cols-4 grid-rows-12 gap-4 bg-background w-full h-full'>
+        {/* <div className='grid grid-cols-4 grid-rows-12 gap-4 bg-background w-full h-full'>
             <div className='col-span-1 row-span-6 border-2 border-foreground'></div>
             <div className='col-span-1 row-span-3 border-2 border-foreground'></div>
             <div className='col-span-1 row-span-3 border-2 border-foreground'></div>
@@ -16,6 +16,18 @@ const Highlights = () => {
             <div className='col-span-1 row-span-6 border-2 border-foreground'></div>
             <div className='col-span-1 row-span-3 border-2 border-foreground'></div>
             <div className='col-span-2 row-span-3 border-2 border-foreground'></div>
+        </div> */}
+        <div className='grid grid-cols-2 grid-rows-15 h-[220vh] gap-2'>
+          <div className='col-span-1 row-span-5  border-2 border-foreground'></div>
+          <div className='col-span-1 row-span-2  border-2 border-foreground'></div>
+          <div className='col-span-1 row-span-1  border-2 border-foreground'></div>
+          <div className='col-span-2 row-span-3  border-2 border-foreground'></div>
+          <div className='col-span-1 row-span-1  border-2 border-foreground'></div>
+          <div className='col-span-1 row-span-3  border-2 border-foreground'></div>
+          <div className='col-span-1 row-span-3  border-2 border-foreground'></div>
+          <div className='col-span-1 row-span-3  border-2 border-foreground'></div>
+          <div className='col-span-2 row-span-2  border-2 border-foreground'></div>
+
         </div>
 
     </section>
